8d59b8c38459c405420051b9bfef122e9c17a196
Ben Baert <ben_b@gmx.com>
2016-06-10 22:17:34 +0200

    Deploycommand proper added to site.hs

--- site.hs
+++ site.hs
@@ -5,6 +5,8 @@ import           Hakyll
 
 
 --------------------------------------------------------------------------------
+
+
 main :: IO ()
 main = hakyllWith config $ do
     match "images/*" $ do
@@ -70,32 +72,32 @@ postCtx =
 --------------------------------------------------------------------------------
 config :: Configuration
 config = defaultConfiguration 
-            { deployCommand = "# Temporarily store uncommited changes
-                               git stash\n
-                               \n
-                               # Verify correct branch\n
-                               git checkout develop\n
-                               \n
-                               # Build new files\n
-                               stack exec myblog clean\n
-                               stack exec myblog build\n
-                               \n
-                               # Get previous files\n
-                               git fetch -all\n
-                               git checkout -b master --track origin/master\n
-                               \n
-                               # Overwrite existing files with new files\n
-                               cp -a _site/. .\n
-                               \n
-                               # Commit\n
-                               git add -A\n
-                               git commit -m 'Publish.'\n
-                               \n
-                               # Push\n
-                               git push origin master:master\n
-                               \n
-                               # Restoration\n
-                               git checkout develop\n
-                               git branch -D master\n
-                               git stash pop\n"
+            { deployCommand = "# Temporarily store uncommited changes\n\
+                               \git stash\n\
+                               \\n\
+                               \# Verify correct branch\n\
+                               \git checkout develop\n\
+                               \\n\
+                               \# Build new files\n\
+                               \stack exec myblog clean\n\
+                               \stack exec myblog build\n\
+                               \\n\
+                               \# Get previous files\n\
+                               \git fetch -all\n\
+                               \git checkout -b master --track origin/master\n\
+                               \\n\
+                               \# Overwrite existing files with new files\n\
+                               \cp -a _site/. .\n\
+                               \\n\
+                               \# Commit\n\
+                               \git add -A\n\
+                               \git commit -m 'Publish.'\n\
+                               \\n\
+                               \# Push\n\
+                               \git push origin master:master\n\
+                               \\n\
+                               \# Restoration\n\
+                               \git checkout develop\n\
+                               \git branch -D master\n\
+                               \git stash pop\n"
          }

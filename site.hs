71a09b6199122d070580c13ff41b4ef614fb1215
Ben Baert <ben_b@gmx.com>
2016-06-10 21:56:10 +0200

    Deploycommand; stack.yaml

--- site.hs
+++ site.hs
@@ -6,7 +6,7 @@ import           Hakyll
 
 --------------------------------------------------------------------------------
 main :: IO ()
-main = hakyll $ do
+main = hakyllWith config $ do
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
@@ -66,3 +66,36 @@ postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
 
+
+--------------------------------------------------------------------------------
+config :: Configuration
+config = defaultConfiguration 
+            { deployCommand = "# Temporarily store uncommited changes
+                               git stash\n
+                               \n
+                               # Verify correct branch\n
+                               git checkout develop\n
+                               \n
+                               # Build new files\n
+                               stack exec myblog clean\n
+                               stack exec myblog build\n
+                               \n
+                               # Get previous files\n
+                               git fetch -all\n
+                               git checkout -b master --track origin/master\n
+                               \n
+                               # Overwrite existing files with new files\n
+                               cp -a _site/. .\n
+                               \n
+                               # Commit\n
+                               git add -A\n
+                               git commit -m 'Publish.'\n
+                               \n
+                               # Push\n
+                               git push origin master:master\n
+                               \n
+                               # Restoration\n
+                               git checkout develop\n
+                               git branch -D master\n
+                               git stash pop\n"
+         }
